698bb03b5eeb5c31debf923683df414a9f2638e8
박해종 <tgic01@korea.ac.kr>
2023-08-30 12:12:36 +0900

    Fix

--- src/component/Web.tsx
+++ src/component/Web.tsx
@@ -19,7 +19,7 @@ const Web: React.FC = () => {
         <Slider {...settings}>
         <div className='Introduce_1' >
             <div style={{ marginRight: "30px" }}>
-                <img src="image/web1.png" alt="web1" /><br/><br/>
+                <img src="/image/web1.png" alt="web1" /><br/><br/>
                 <a href="https://github.com/veblen1/ssafy-public/tree/main/1%ED%95%99%EA%B8%B0%20%EA%B4%80%ED%86%B5%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8" target="_blank" rel="noopener noreferrer">
                     GitHub 바로가기
                 </a>
@@ -27,7 +27,7 @@ const Web: React.FC = () => {
         </div>
         <div className='Introduce_1'>
             <div style={{ marginRight: "30px" }}>
-                <img src="image/web2.png" alt="web2" /><br/><br/>
+                <img src="/image/web2.png" alt="web2" /><br/><br/>
                 <a href="https://github.com/veblen1/ssafy-public/tree/main/2%ED%95%99%EA%B8%B0%20%EA%B3%B5%ED%86%B5%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8" target="_blank" rel="noopener noreferrer">
                     GitHub 바로가기
                 </a>
